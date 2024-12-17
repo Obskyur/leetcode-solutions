76dad213ae07d0a17ea3a1aff3b09f817b2de0d8
Tristan Miller <118584664+Obskyur@users.noreply.github.com>
2024-12-16 19:28:48 -0800

    Time: 12 ms (29.02%), Space: 51.5 MB (5.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.cs
+++ 0169-majority-element/0169-majority-element.cs
@@ -1,13 +1,5 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
-        var freq = nums.GroupBy(num => num);
-        int max = -1, retval = -1;
-        foreach(var group in freq) {
-            if (group.Count() > max) {
-                max = group.Count();
-                retval = group.Key;
-            }
-        }
-        return retval;
+        return nums.GroupBy(num => num).OrderByDescending(group => group.Count()).First().Key;
     }
 }
