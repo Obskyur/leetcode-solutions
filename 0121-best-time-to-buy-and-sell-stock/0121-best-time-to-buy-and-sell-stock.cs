7d14ab7ed14d877ed2ec91250ea9cd998212dd03
Tristan Miller <118584664+Obskyur@users.noreply.github.com>
2024-12-16 19:56:32 -0800

    Time: 1 ms (100%), Space: 58.5 MB (24.55%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
@@ -2,7 +2,7 @@ public class Solution {
     public int MaxProfit(int[] prices) {
         int min = int.MaxValue, diff = -1;
         foreach(var price in prices) {
-            min = Math.Min(min, price);;
+            min = Math.Min(min, price);
             diff = Math.Max(diff, price - min);
         };
         return diff;
