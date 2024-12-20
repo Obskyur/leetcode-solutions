ecae74d47696317bc41a6f6ce0a58d6ccc3e428d
Tristan Miller <118584664+Obskyur@users.noreply.github.com>
2024-12-19 22:18:38 -0800

    Time: 64 ms (13.5%), Space: 45.5 MB (35.86%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool CanConstruct(string ransomNote, string magazine) {
+        var magLetters = new Dictionary<char, int>();
+
+        foreach (char c in magazine) {
+            if (magLetters.ContainsKey(c)) {
+                magLetters[c]++;
+            }
+            else {
+                magLetters.Add(c, 1);
+            }
+        };
+
+        foreach (char c in ransomNote) {
+            if (magLetters.ContainsKey(c)) {
+                if (--magLetters[c] == 0) {
+                    magLetters.Remove(c);
+                }
+            }
+            else {
+                return false;
+            }
+        };
+
+        return true;
+    }
+}
