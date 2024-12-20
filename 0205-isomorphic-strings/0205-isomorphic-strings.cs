f9d1908613d2492376ada28f5741a381e9e8bd47
Tristan Miller <118584664+Obskyur@users.noreply.github.com>
2024-12-20 02:16:14 -0800

    Time: 18 ms (15.87%), Space: 43 MB (23.93%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cs
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cs
@@ -1,7 +1,6 @@
 public class Solution {
     public bool IsIsomorphic(string s, string t) {
         var mappings = new Dictionary<char, char>();
-        string sCopy = string.Copy(s);
 
         if (s.Length != t.Length) {
             return false;
