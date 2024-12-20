9798ec06c8840ac56b068a40dc0e6069078943bb
Tristan Miller <118584664+Obskyur@users.noreply.github.com>
2024-12-20 01:51:32 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cs
+++ 0383-ransom-note/0383-ransom-note.cs
@@ -1,10 +1,21 @@
 public class Solution {
     public bool CanConstruct(string ransomNote, string magazine) {
-        var magLetters = new List<char>(magazine);
+        var magLetters = new Dictionary<char, int>();
+
+        foreach (char c in magazine) {
+            if (magLetters.ContainsKey(c)) {
+                magLetters[c]++;
+            }
+            else {
+                magLetters.Add(c, 1);
+            }
+        };
 
         foreach (char c in ransomNote) {
             if (magLetters.Contains(c)) {
-                magLetters.Remove(c);
+                if (--magLetters[c] == 0) {
+                    magLetters.Remove(c);
+                }
             }
             else {
                 return false;
