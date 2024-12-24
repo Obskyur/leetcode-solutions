eb459d21ada43152b6e871ec8c5d72b8aa068163
Tristan Miller <118584664+Obskyur@users.noreply.github.com>
2024-12-24 13:03:16 -0800

    Time: 2 ms (57.37%), Space: 46.7 MB (41.89%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cs
+++ 0228-summary-ranges/0228-summary-ranges.cs
@@ -1,19 +1,26 @@
 public class Solution {
     public IList<string> SummaryRanges(int[] nums) {
-        var outList = new List<string>();        
-        int start, end;
+        var outList = new List<string>();
 
         for (int i = 0; i < nums.Length; i++) {
-            start = nums[i];
-            while (i < nums.Length - 1 && nums[i + 1] - nums[i] == 1)
-                i++;
-            end = nums[i];
-            
-            if (start == end)
-                outList.Add($"{start}");
-            else
-                outList.Add($"{start}->{end}");
+            var (start, end) = GetRange(nums, ref i);
+            AddRangeToList(outList, (start, end));
         }
         return outList;
+    }
+
+    private (int, int) GetRange(int[] nums, ref int i) {
+        int start = nums[i];
+        while (i < nums.Length - 1 && nums[i + 1] - nums[i] == 1)
+            i++;
+        int end = nums[i];
+        return (start, end);
+    }
+
+    private void AddRangeToList(List<string> list, (int start, int end) range) {
+            if (range.start == range.end)
+                list.Add($"{range.start}");
+            else
+                list.Add($"{range.start}->{range.end}");
     }
 }
