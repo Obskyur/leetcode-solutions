843ff6236dbf49de2e5d41b53309bfbc034357bc
Tristan Miller <118584664+Obskyur@users.noreply.github.com>
2024-12-30 17:55:03 -0800

    Time: 2 ms (83.12%), Space: 41.1 MB (46.44%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cs
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -2,21 +2,22 @@ public class Solution {
     public bool IsValid(string s) {
         var pStack = new Stack<char>();
         foreach (var c in s) {
-            if (c == '(') {
-                pStack.Push(')');
-                continue;
+            switch (c) {
+                case '(': 
+                    pStack.Push(')');
+                    break;
+                case '{':
+                    pStack.Push('}');
+                    break;
+                case '[':
+                    pStack.Push(']');
+                    break;
+                default:
+                    if (pStack.Count == 0 || pStack.Pop() != c)
+                        return false;
+                    break;
             }
-            if (c == '{') {
-                pStack.Push('}');
-                continue;
-            }
-            if (c == '[') {
-                pStack.Push(']');
-                continue;
-            }
-            if (pStack.Count == 0 || pStack.Pop() != c)
-                return false;
-        };
+        }
         return pStack.Count == 0;
     }
 }
