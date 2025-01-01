0f01e30eaa4546454e3baf4cac3d7056f97d51a8
Tristan Miller <118584664+Obskyur@users.noreply.github.com>
2024-12-31 20:14:27 -0800

    Time: 1 ms (89.91%), Space: 53.3 MB (36.4%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cs
+++ 0002-add-two-numbers/0002-add-two-numbers.cs
@@ -10,46 +10,11 @@
  * }
  */
 public class Solution {
-    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-        var retHead = new ListNode();
-        ListNode p1 = l1, 
-            p2 = l2, 
-            tail = retHead;
-        bool carry = false;
-
-        while (p1 != null && p2 != null) {
-            int sum = p1.val + p2.val + (carry ? 1 : 0);
-            if (sum >= 10) {
-                carry = true;
-                sum %= 10;
-            }
-            else
-                carry = false;
-            tail.next = new ListNode(sum);
-            p1 = p1.next;
-            p2 = p2.next;
-            tail = tail.next;
-        }
-        var remainingNums = p1 ?? p2;
-        tail.next = remainingNums;
-        while (carry == true) {
-            if (remainingNums == null) {
-                tail.next = new ListNode(1);
-                tail = tail.next;
-                carry = false;
-            }
-            else if (remainingNums.val == 9) {
-                remainingNums.val = 0;
-                remainingNums = remainingNums.next;
-                tail = tail.next;
-            }
-            else if (remainingNums.val < 9) {
-                remainingNums.val += 1;
-                remainingNums = remainingNums.next;
-                tail = tail.next;
-                carry = false;
-            }
-        }
-        return retHead.next;
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2, byte carry = 0) {
+        if ((l1, l2, carry) is (null, null, 0))
+            return null;
+        int sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+        carry = (byte)(sum / 10);
+        return new ListNode(sum % 10, AddTwoNumbers(l1?.next, l2?.next, carry));
     }
 }
