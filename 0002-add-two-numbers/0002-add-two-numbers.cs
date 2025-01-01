83f957299612be0b8bb6f297fcddd7548a74fe52
Tristan Miller <118584664+Obskyur@users.noreply.github.com>
2024-12-31 20:16:10 -0800

    Time: 1 ms (89.91%), Space: 53.6 MB (14.65%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cs
+++ 0002-add-two-numbers/0002-add-two-numbers.cs
@@ -10,11 +10,20 @@
  * }
  */
 public class Solution {
-    public ListNode AddTwoNumbers(ListNode l1, ListNode l2, byte carry = 0) {
-        if ((l1, l2, carry) is (null, null, 0))
-            return null;
-        int sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
-        carry = (byte)(sum / 10);
-        return new ListNode(sum % 10, AddTwoNumbers(l1?.next, l2?.next, carry));
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode retHead = new ListNode();
+        ListNode curNode = retHead;
+        byte carry = 0;
+
+        while ((l1, l2, carry) is not (null, null, 0)) {
+            int sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+            carry = (byte)(sum / 10);
+            curNode.next = new ListNode(sum % 10);
+            l1 = l1?.next;
+            l2 = l2?.next;
+            curNode = curNode.next;
+        }
+
+        return retHead.next;
     }
 }
